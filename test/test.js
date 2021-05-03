54da2a7e96413eacab9592fd4cb231171d1c73c1
ARIJIT <arijit.mahapatra@tongadive.com>
2021-05-03 18:35:42 +0530

    test file modifed

--- test/test.js
+++ test/test.js
@@ -4,6 +4,6 @@ const app = require('../app.js');
 describe('GET /', () => {
     it('respond with hello world', (done) => {
     //navigate to root and check the the response is "hello world"
-    request(app).get('/api').expect('Hello Node', done);
+    request(app).get('/').expect('Hello Node', done);
     });
 });
