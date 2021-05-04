3efde6e06bcbc758e72296ff753a6dc29550a867
ARIJIT <arijit.mahapatra@tongadive.com>
2021-05-04 13:30:49 +0530

    modified-v2

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/sh     
-ssh jenkins@40.114.52.166 <<EOF
+ssh jenkins@40.114.52.166
 git clone https://github.com/arijitmhptr/demoapp.git
-cd demoapp  
+cd demoapp 
 npm install --production
-sudo npm install pm2 -g      
-pm2 start index.js --name node-app
-exit      
-EOF
+sudo npm install pm2 -g
+pm2 start app.js --name node-app
